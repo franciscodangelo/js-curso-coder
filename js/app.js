7fc387194088c80c34c38e0eb4f15a62f02c3908
Francisco Dangelo <fgabrieldangelo@gmail.com>
2022-10-23 22:02:36 -0300

    prueba final

--- js/app.js
+++ js/app.js
@@ -1,42 +1,53 @@
-//ARRAY DE OBJETOS ("BASE DE DATOS") CARGA DE DATA.JSON
-
-async function fetchProductos() {
-    const response = await fetch('./data.json')
-    return await response.json()
-}
-
 let productos = [];
-
-fetchProductos().then(product => {
-    productos = product
-    mostrarProductos()
-})
-
-
-//CONSTRUCTOR - AGREGADO DE ELEMENTO CANTIDAD - CUANDO PUSHEAMOS AL CARRITO 
-
-class ProductoEnCarrito {
-    constructor(prod) {
-        this.producto = prod;
-        this.cantidad = 1;
-}
-}
-
-
-//ARRAY DE CARRITO VACIO
-
-
 let carrito = [];
 
 
-//MOSTRAR PRODUCTOS EN HTML 
+async function fetchProductos() {
+  const response = await fetch("./data.json");
+  return await response.json();
+}
+
+fetchProductos().then((product) => {
+  productos = product;
+  mostrarProductos();
+});
+
+//CONSTRUCTOR - AGREGADO DE ELEMENTO CANTIDAD - (PUSH AL CARRITO)
+
+class ProductoEnCarrito {
+  constructor(prod) {
+    this.producto = prod;
+    this.cantidad = 1;
+  }
+}
+
+//FUNCIONES LOCAL STORAGE (JSON)
+function addLocalStorage() {
+  const carritoStr = JSON.stringify(carrito);
+  localStorage.setItem("carrito", carritoStr);
+}
+
+function traerDeLocal() {
+  const carroVuelta = localStorage.getItem("carrito");
+  carroVuelta !== null && (carrito = JSON.parse(carroVuelta));
+  mostrarCarrito();
+}
+
+
+//MOSTRAR PRODUCTOS 
 
 const mostrarProductos = () => {
-const seccionProductos = document.getElementById("seccion-productos");
-    productos.forEach((producto) => {
-        const card = document.createElement("card");
-        card.classList.add('col-12', 'col-md-4', 'col-xl-3', 'd-flex', 'justify-content-center')
-        card.innerHTML += `<div class="card border border-dark m-2 align-items-center bg-light" style="width: 18rem;">
+  const seccionProductos = document.getElementById("seccion-productos");
+  productos.forEach((producto) => {
+    const card = document.createElement("card");
+    card.classList.add(
+      "col-12",
+      "col-md-4",
+      "col-xl-3",
+      "d-flex",
+      "justify-content-center"
+    );
+    card.innerHTML += `<div class="card border border-dark m-2 align-items-center bg-light" style="width: 18rem;">
             <img src="${producto.img}" class="card-img-top p-3" alt="imagenes de la calle">
             <div class="card-body">
             <h5 class="card-title">${producto.nombre}: ${producto.detalle}</h5>
@@ -45,80 +56,98 @@ const seccionProductos = document.getElementById("seccion-productos");
             </div>
         </div>`;
 
-        seccionProductos.appendChild(card);
+    seccionProductos.appendChild(card);
 
-//EVENTO CLICK PARA CARGAR AL CARRITO CON CONDICION (SI EXISTE EN CARRITO SOLO CARGA + UNIDAD DE PRODUCTO SINO PUSHEA PRODUCTO A CARRITO)
 
-const button = document.getElementById(`button${producto.id}`);
+    const button = document.getElementById(`button${producto.id}`);
 
-        button.addEventListener("click", () => {
-        const index = carrito.findIndex((p) => p.producto.id === producto.id);
-        if (index !== -1) {
-            const p = carrito[index];
-            productoAdd(p);
-            addLocalStorage();
-            mostrarCarrito();
-            return;
-}
+    button.addEventListener("click", () => {
+      const index = carrito.findIndex((p) => p.producto.id === producto.id);
+      if (index !== -1) {
+        const p = carrito[index];
+        productoAdd(p);
+        addLocalStorage();
+        mostrarCarrito();
+        return;
+      }
 
-const newProduct = new ProductoEnCarrito(producto);
-carrito.push(newProduct);
+      const newProduct = new ProductoEnCarrito(producto);
+      carrito.push(newProduct);
 
-addLocalStorage();
-mostrarCarrito();
-
-});
-});
+      addLocalStorage();
+      mostrarCarrito();
+    });
+  });
 };
 
+mostrarProductos(productos);
+traerDeLocal();
 
-//FUNCIONES
 
-//funcion para mostrar el carrito con totales parciales por producto - total general - boton para vaciar producto
+
+// MOSTRAR CARRITO + (form)
 
 function mostrarCarrito() {
-const section = document.getElementById("seccion-carrito");
-section.innerHTML = "";
+  const section = document.getElementById("seccion-carrito");
+  section.innerHTML = "";
 
-
-let total = 0;
-carrito.forEach((p) => {
-      total += p.producto.precio * p.cantidad;
-        section.innerHTML += `
-            <div>
-              <p>${p.cantidad} x ${p.producto.nombre}: $${p.producto.precio * p.cantidad}</p>
-
-            <button class="carrito-add-one btn btn-success" onclick="sumarUnoAProducto(${p.producto.id})">+</button>
+  let total = 0;
+  carrito.forEach((p) => {
+    total += p.producto.precio * p.cantidad;
+    section.innerHTML += `
+          <div class="border p-4 mt-3 d-flex align-items-center">
+            <p class="fs-5 me-3">${p.producto.nombre} (${p.producto.detalle}) x ${p.cantidad} =  $${p.producto.precio * p.cantidad}</p>
+            <button class="carrito-add-one btn btn-success me-1" onclick="sumarUnoAProducto(${p.producto.id})">+</button>
             <button class="carrito-remove-one ms-1 btn btn-danger" onclick="restarUnoAProducto(${p.producto.id})">-</button>
-
-            </div>
+          </div>
             `;
-            addLocalStorage();
+    addLocalStorage();
+  });
 
-});
+  section.innerHTML += `<p class="mt-5 mb-5 fs-3 fw-bold">Total: $${total}</p>`;
+  section.innerHTML += `<a class="btn btn-success" id="comprar">Comprar</a>`;
+  section.innerHTML += `<a class="btn btn-danger ms-2" id="buttonVaciar">Vaciar Carrito</a>`;
 
-section.innerHTML += `<p>Total: ${total}</p>`;
-section.innerHTML += `<a class="btn btn-success" id="finalizarCompra">Finalizar Compra</a>`
-section.innerHTML += `<a class="btn btn-danger ms-2" id="buttonVaciar">Vaciar Carrito</a>`
-
-
-const buttonV = document.querySelector('#buttonVaciar')
-buttonV.addEventListener("click", () => {
+  const buttonV = document.getElementById("buttonVaciar");
+  buttonV.addEventListener("click", () => {
     vaciarCarrito();
     addLocalStorage();
-});
+  });
 
-const buttonF = document.querySelector('#finalizarCompra')
+  const buttonF = document.getElementById("comprar");
+  const formulario = document.getElementById("formulario");
 
-buttonF.addEventListener("click", () => {
-    if (carrito == 0){
-        Swal.fire({
-            title: 'Su Carrito esta Vacio',
-            icon: 'warning',
-        })
-    }else{
+  buttonF.addEventListener("click", () => {
+    if (carrito.length === 0) {
+      Swal.fire({
+        title: "Su Carrito esta Vacio",
+        icon: "warning",
+      });
+      return;
+    }
+
+    formulario.classList.remove("d-none");
+    window.scrollTo({ top: 200000 });
+  });
+
+  formulario.addEventListener("submit", ev => {
+    ev.preventDefault();
+
+    const data = new FormData(formulario);
+
+    const nombre = data.get("nombre");
+    const apellido = data.get("apellido");
+    const direccion = data.get("direccion");
+
+    if (carrito.length === 0) {
+      Swal.fire({
+        title: "Su Carrito esta Vacio",
+        icon: "warning",
+      });
+      return;
+    }
     Swal.fire({
-        title: 'Esta seguro/a que quiere finalizar su compra?',
+        title: `Seguro/a ${nombre} ${apellido} que quiere Finalizar su Compra?`,
         icon: 'question',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -129,80 +158,56 @@ buttonF.addEventListener("click", () => {
             vaciarCarrito();
             addLocalStorage();
             Swal.fire(
-            'Compra Finalizada',
-            'Muchas Gracias por confiar en Nosotros',
+            `Muchas Gracias ${nombre} ${apellido} por realizar su Compra`,
+            `El pedido llegará en el transcurso de los próximos 10 días a la siguiente dirección: ${direccion}`,
             'success'
-        )
+            );
+            formulario.classList.add("d-none");
+            formulario.reset();
         }
     })
-}
-});
-
+  });
 }
 
-//FUNCION VACIAR EL CARRITO DE COMPRA
+
+// FUNCION vaciar-agregarP-quitarP-sustraerC-adherirC (carrito)
 
 function vaciarCarrito() {
-    carrito = [];
-    mostrarCarrito();
+  carrito = [];
+  mostrarCarrito();
 }
 
-
-//FUNCION PARA AGREGAR UNA UNIDAD DE PRODUCTO
 
 function productoAdd(p) {
-    p.cantidad ++;
+  p.cantidad++;
 }
-
-
-//FUNCION PARA QUITAR UNA UNIDAD DE PRODUCTO
-
-function subtract(p) {
-    p.cantidad !==0 && p.cantidad--;
-}
-
-
-//FUNCION PARA AGREGAR UNIDAD CON CHEQUEO DE ID DE PRODUCTO
 
 function sumarUnoAProducto(id) {
-    const index = carrito.findIndex((p) => p.producto.id === id);
-    const producto = carrito[index];
+  const index = carrito.findIndex((p) => p.producto.id === id);
+  const producto = carrito[index];
 
-    productoAdd(producto);
-    mostrarCarrito();
+  productoAdd(producto);
+  mostrarCarrito();
 }
 
-
-//FUNCION PARA QUITAR UNIDAD CON CHEQUEO DE ID DE PRODUCTO
+function subtract(p) {
+  p.cantidad !== 0 && p.cantidad--;
+}
 
 function restarUnoAProducto(id) {
-    const index = carrito.findIndex((p) => p.producto.id === id);
-    const producto = carrito[index];
+  const index = carrito.findIndex((p) => p.producto.id === id);
+  const producto = carrito[index];
 
-    subtract(producto);
-        if (producto.cantidad == 0) {
-            carrito = carrito.filter((p) => p.producto.id !== id);
-}
+  subtract(producto);
+  if (producto.cantidad == 0) {
+    carrito = carrito.filter((p) => p.producto.id !== id);
+  }
 
-mostrarCarrito();
-}
-
-
-//FUNCIONES LOCAL STORAGE (JSON)
-function addLocalStorage(){
-    const carritoStr = JSON.stringify(carrito)
-    localStorage.setItem("carrito", carritoStr)
-}
-
-function traerDeLocal(){
-    const carroVuelta = localStorage.getItem("carrito")
-    carroVuelta !== null && (carrito = JSON.parse(carroVuelta));
-    mostrarCarrito();
+  mostrarCarrito();
 }
 
 
-mostrarProductos(productos);
-traerDeLocal();
+
 
 
 
